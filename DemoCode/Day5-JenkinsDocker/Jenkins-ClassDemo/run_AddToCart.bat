db7bb88e1abdbc9b80b592a40cd92842a704931e
Anjali Yogesh Kulkarni <49025481+anjaliyogeshkulkarni@users.noreply.github.com>
2025-05-10 11:31:57 +0530

    job files

---
+++ DemoCode/Day5-JenkinsDocker/Jenkins-ClassDemo/run_AddToCart.bat
@@ -0,0 +1,19 @@
+@echo off
+
+cd /d D:\Anjali\CourseMaterial\Playwright\DemoCode-PlaywrightBasics\Day4\Login_Behave
+
+python -m venv venv
+
+call venv\Scripts\activate
+
+pip install -r requirements.txt
+
+python -m playwright install
+
+C:\Users\dell\anaconda3\envs\DemoCode\Scripts\behave ./features/Products.feature -f allure_behave.formatter:AllureFormatter -o C:\jenkins_reports\allure-results
+
+allure generate C:\jenkins_reports\allure-results -o C:\jenkins_reports\allure-report --clean
+
+allure open C:\jenkins_reports\allure-report
+
+deactivate
